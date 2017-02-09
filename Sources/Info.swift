91f401d8047d377d995a30aee775de7596bad31e
Tamara Cook <tamara.cook@icloud.com>
2017-02-09 17:47:44 +0100

    Create OptionSet for SANE_Info

--- Sources/Info.swift
+++ Sources/Info.swift
@@ -1,9 +1,6 @@
-//
-//  Info.swift
-//  libsane
-//
-//  Created by Tamara Cook on 07.02.17.
-//
-//
-
-import Foundation
+struct Info: OptionSet {
+  let rawValue: Int
+  static let inexact = Info(rawValue: 1 << 0)
+  static let reloadOptions = Info(rawValue: 1 << 1)
+  static let reloadParams = Info(rawValue: 1 << 2)
+}

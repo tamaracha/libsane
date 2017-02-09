d11103c8dda8579708abe48d8bfd4265d25bbedf
Tamara Cook <tamara.cook@icloud.com>
2017-02-09 17:45:43 +0100

    Put Clibsane dependency out of edit mode

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "libsane",
     dependencies: [
-        .Package(url: "../Clibsane", majorVersion: 1)
+        .Package(url: "https://github.com/tamaracha/Clibsane", majorVersion: 1)
     ]
 )

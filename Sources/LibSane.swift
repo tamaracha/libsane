ab6f75fb6efbd590bc41bab73d6661a5e05a9fa0
Tamara Cook <tamara.cook@icloud.com>
2017-02-11 19:35:39 +0100

    Document LibSane

--- Sources/LibSane.swift
+++ Sources/LibSane.swift
@@ -1,14 +1,16 @@
 import Clibsane
 
+/// The entry class for this module
 final public class LibSane {
-  /// Singleton due to hardware communication
+  /// An instance of LibSane itself. It inits and exits the SANE backend during init and deinit.
   static let shared = LibSane()
 
   //MARK: Properties
+  /// The version of SANE. It is set during initialization.
   let version: Version
 
   //MARK: Lifecycle hooks
-  init?() {
+  private init?() {
     var version: Int32 = 0
     let status = Int(sane_init(&version, nil).rawValue)
     guard status == 0 else {
@@ -21,6 +23,12 @@ final public class LibSane {
   }
 
   //MARK: Static methods
+  /**
+    This function can be used to search for connected SANE devices
+
+   - parameter localOnly: skips network devices in search, defaults to false
+    - returns: the found devices as a dictionary, where a device can be accessed by its name
+  */
   public static func getDevices(localOnly: Bool = false) throws -> [String: Device] {
     guard shared != nil else {
       throw SaneStatus.notInitialized

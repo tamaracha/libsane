d7065aa27e44d96fb43c0d1a1612c06272c870e2
Tamara Cook <tamara.cook@icloud.com>
2017-02-28 17:20:19 +0100

    Define Parameters struct

--- Sources/Parameters.swift
+++ Sources/Parameters.swift
@@ -1,9 +1,39 @@
-//
-//  Parameters.swift
-//  libsane
-//
-//  Created by Tamara Cook on 28.02.17.
-//
-//
+import Clibsane
 
-import Foundation
+public struct Parameters {
+  public enum Frame {
+    case gray, rgb, red, green, blue
+    init?(_ saneValue: SANE_Frame) {
+      switch saneValue {
+      case SANE_FRAME_GRAY: self = .gray
+      case SANE_FRAME_RGB: self = .rgb
+      case SANE_FRAME_RED: self = .red
+      case SANE_FRAME_GREEN: self = .green
+      case SANE_FRAME_BLUE: self = .blue
+      default: return nil
+      }
+    }
+  }
+
+  public let format: Frame
+  public let lastFrame: Bool
+  public let bytesPerLine: Int
+  public let pixelsPerLine: Int
+  public let lines: Int
+  public let depth: Int
+  public var bytesTotal: Int {
+    return lines*bytesPerLine
+  }
+  public var bytesPerSample: Int {
+    return (depth <= 8) ? 1 : 2
+  }
+
+  init(_ saneValue: SANE_Parameters) {
+    format = Frame(saneValue.format)!
+    lastFrame = saneValue.last_frame == SANE_TRUE
+    bytesPerLine = Int(saneValue.bytes_per_line)
+    pixelsPerLine = Int(saneValue.pixels_per_line)
+    lines = Int(saneValue.lines)
+    depth = Int(saneValue.depth)
+  }
+}

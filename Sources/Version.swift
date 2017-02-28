3279b2b7dc8ed5876454c92a2900c63a0c57d6fc
Tamara Cook <tamara.cook@icloud.com>
2017-02-28 14:37:41 +0100

    Use SANE_Int in Version.init

--- Sources/Version.swift
+++ Sources/Version.swift
@@ -1,9 +1,11 @@
-/// This represents the version of the SANE api. It consists of major, minor and build number.
+import Clibsane
+
+/// This represents the version returned by the SANE api. It consists of major, minor and build number.
 public struct Version {
   let major: Int, minor: Int, build: Int
 
-  ///  SANE stores its version info in an Int32 which is decomposed here into major (first byte), minor (second byte), and build (remaining bytes).
-  init(_ n: Int32) {
+  ///  SANE stores its version info in an SANE_Int which is decomposed here into major (first byte), minor (second byte), and build (remaining bytes).
+  init(_ n: SANE_Int) {
     major = Int((n >> 24) & 0xff)
     minor = Int((n >> 16) & 0xff)
     build = Int((n >> 0) & 0xffff)

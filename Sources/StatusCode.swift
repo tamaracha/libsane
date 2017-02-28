7a6dc8866e739a614d6f44cc467741da7f82e92f
Tamara Cook <tamara.cook@icloud.com>
2017-03-01 00:53:54 +0100

    Add documentation to SANE_Status extension

--- Sources/StatusCode.swift
+++ Sources/StatusCode.swift
@@ -1,5 +1,6 @@
 import Clibsane
 
+/// The status codes returned by SANE
 extension SANE_Status: CustomStringConvertible, Error {
   /// Use the textual status representation provided by SANE as description
   public var description: String {
